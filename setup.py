2b3a7d86e79ebe588ef919460745c0392fae76ff
Geert Jansen <geertj@gmail.com>
2010-06-20 13:41:30 -0400

    Specify dependencies.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     package_dir = {'': 'lib'},
     py_modules = ['pexpect', 'winpexpect'],
     test_suite = 'nose.collector',
+    install_requires = ['pywin32 >= 214', 'nose' ],
     zip_safe = False,
     use_2to3 = True
 )
